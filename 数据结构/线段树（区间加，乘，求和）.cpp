8a75ce8a5f6547c40c89933414c1409ec849fb04
rhp-QE <60916009+rhp-QE@users.noreply.github.com>
2021-09-05 22:50:11 +0800

    Update 线段树（区间加，乘，求和）.cpp

--- 数据结构/线段树（区间加，乘，求和）.cpp
+++ 数据结构/线段树（区间加，乘，求和）.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #define lowbit(x) x&(-x)
-#define l(o) (o<<1)
-#define r(o) (o<<1)|1
+#define lo (o<<1)
+#define ro (o<<1)|1
 #define rep(i,a,b) for(int i=a;i<=b;i++)
 #define mem(a,b) memset(a,b,sizeof(a))
 #define inf 1LL<<60
@@ -30,28 +30,30 @@ struct node {
 	ll sum;              //定义维护信息
 	ll opa, opm;         //定义懒惰标记
 };
-node a[maxn*4];
+node a[maxn * 4];
 
-void pushdown(int o) {   
+void calc_mult(int o,ll op) {
+	a[o].opa = a[o].opa * op % md; a[o].opm = a[o].opm * op % md;
+	a[o].sum = a[o].sum * op % md;
+}
+void calc_add(int o, ll op) {
+	a[o].opa = (a[o].opa + op) % md;
+	a[o].sum = (a[o].sum + (a[o].r - a[o].l + 1) * op) % md;
+}
+void pushdown(int o) {
 	if (a[o].opm != 1) {
 		ll& op = a[o].opm;
-		a[l(o)].opa = a[l(o)].opa * op % md; a[l(o)].opm = a[l(o)].opm * op % md;
-		a[l(o)].sum = a[l(o)].sum * op % md;
-		a[r(o)].opa = a[r(o)].opa * op % md; a[r(o)].opm = a[r(o)].opm * op % md;
-		a[r(o)].sum = a[r(o)].sum * op % md;
+		calc_mult(lo, op); calc_mult(ro, op);
 		op = 1;
 	}
 	if (a[o].opa != 0) {
 		ll& op = a[o].opa;
-		a[l(o)].opa = (a[l(o)].opa + op) % md; 
-		a[l(o)].sum = (a[l(o)].sum + (a[l(o)].r - a[l(o)].l + 1) * op) % md;
-		a[r(o)].opa = (a[r(o)].opa + op) % md;
-		a[r(o)].sum = (a[r(o)].sum + (a[r(o)].r - a[r(o)].l + 1) * op) % md;
+		calc_add(lo, op);  calc_add(ro, op);
 		op = 0;
 	}
 }
 void pushup(int o) {
-	a[o].sum = a[l(o)].sum + a[r(o)].sum;
+	a[o].sum = a[lo].sum + a[ro].sum;
 }
 void init(int l, int r, int o) {
 	a[o].l = l; a[o].r = r; a[o].opa = 0; a[o].opm = 1;
@@ -59,34 +61,31 @@ void init(int l, int r, int o) {
 		a[o].sum = num[l];  return;
 	}
 	int m = l + ((r - l) >> 1);
-	init(l, m, l(o));
-	init(m + 1, r, r(o));
+	init(l, m, lo);
+	init(m + 1, r, ro);
 	pushup(o);
 }
 void update_add(int o) {
 	if (ql <= a[o].l && a[o].r <= qr) {
-		a[o].sum = (a[o].sum + v * (a[o].r - a[o].l + 1)) % md;
-		a[o].opa = (a[o].opa + v) % md;
+		calc_add(o, v);
 		return;
 	}
 	pushdown(o);
 	int m = a[o].l + ((a[o].r - a[o].l) >> 1);
-	if (ql <= m) update_add(l(o));
-	if (qr > m) update_add(r(o));
+	if (ql <= m) update_add(lo);
+	if (qr > m) update_add(ro);
 	pushup(o);
 }
 
 void update_mult(int o) {
 	if (ql <= a[o].l && a[o].r <= qr) {
-		a[o].sum = a[o].sum * v % md;
-		a[o].opa = a[o].opa * v % md;
-		a[o].opm = a[o].opm * v % md;
+		calc_mult(o, v);
 		return;
 	}
 	pushdown(o);
 	int m = a[o].l + ((a[o].r - a[o].l) >> 1);
-	if (ql <= m) update_mult(l(o));
-	if (qr > m) update_mult(r(o));
+	if (ql <= m) update_mult(lo);
+	if (qr > m) update_mult(ro);
 	pushup(o);
 }
 
@@ -95,8 +94,8 @@ ll query(int o) {
 	pushdown(o);
 	int m = a[o].l + ((a[o].r - a[o].l) >> 1);
 	ll ans = 0;
-	if (ql <= m) ans += query(l(o));
-	if (qr > m) ans += query(r(o));
+	if (ql <= m) ans += query(lo);
+	if (qr > m) ans += query(ro);
 	pushup(o);
 	return ans;
 }
@@ -123,62 +122,3 @@ int main() {
 		}
 	}
 }
-
-//------------------------------------------------------------------
-const int maxn = 2e6 + 11;
-int ql, qr;
-ll val, md;
-struct Seg {
-	struct Tr {
-		int l, r;
-		ll sum;
-		ll lazy;
-	}tr[maxn];
-	//! @brief 懒惰标记下放需要的计算
-	void calc(int v) {
-		tr[v].sum += val * (tr[v].r - tr[v].l + 1);
-		tr[v].lazy += val;
-	}
-	void pushup(int v) {
-		tr[v].sum += tr[lv].sum + tr[rv].sum;
-	}
-	void pushdown(int v) {
-		if (tr[v].lazy != 0) {
-			val = tr[v].lazy;  tr[v].lazy = 0;
-			calc(lv);
-			calc(rv);
-		}
-	}
-	void build(int v, int l, int r) {
-		tr[v].sum = tr[v].lazy = 0;
-		tr[v].l = l;  tr[v].r = r;
-		if (l == r)  return;
-		int mid = l + ((r - l) >> 1);
-		build(lv, l, mid);
-		build(rv, mid + 1, r);
-	}
-	void set(int l, int r, int v) {
-		ql = l; qr = r; val = v;
-	}
-	ll query(int v) {
-		if (ql <= tr[v].l && tr[v].r <= qr) return tr[v].sum;
-		pushdown(v);
-		int mid = tr[v].l + ((tr[v].r - tr[v].l) >> 1);
-		ll ans = 0;
-		if (ql <= mid)   ans += query(lv);
-		if (qr > mid)  ans += query(rv);
-		pushup(v);
-		return ans;
-	}
-	void update(int v) {
-		if (ql <= tr[v].l && tr[v].r <= qr) {
-			calc(v);
-			return;
-		}
-		pushdown(v);
-		int mid = tr[v].l + ((tr[v].r - tr[v].l) >> 1);
-		if (ql <= mid) update(lv);
-		if (qr > mid)  update(rv);
-		pushup(v);
-	}
-}s1,s2,s3;
